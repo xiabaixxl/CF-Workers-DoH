75b55ee3bad47d03250b12915ef87ee835a6816f
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-09-10 03:46:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: 0BSD
 
 // 定义DNS over HTTPS (DoH)的端点
-const doh = 'https://dns.alidns.com/dns-query'
-const dohjson = 'https://dns.alidns.com/dns-query'
+const doh = 'https://doh.pub/dns-query'
+const dohjson = 'https://doh.pub/resolve'
 
 // 定义内容类型
 const contype = 'application/dns-message'
@@ -17,45 +17,37 @@ const r404 = new Response(null, {status: 404});
 // 导出默认的fetch处理函数
 export default {
     async fetch(request, env, ctx) {
-        return handleRequest(request);
-    },
+        const { method, headers, url } = request
+        const {searchParams, pathname} = new URL(url)
+        
+        // 检查请求路径是否符合设定的前缀
+        if (!pathname.startsWith(path)) {
+            return r404;
+        }
+
+        // 处理GET请求,用于DNS查询
+        if (method === 'GET' && searchParams.has('dns')) {
+            return fetch(`${doh}?dns=${searchParams.get('dns')}`, {
+                headers: { 'Accept': contype }
+            });
+        } 
+        // 处理POST请求,用于DNS查询
+        else if (method === 'POST' && headers.get('content-type') === contype) {
+            // 直接传输请求体,无需等待
+            return fetch(doh, {
+                method: 'POST',
+                headers: {
+                    'Accept': contype,
+                    'Content-Type': contype,
+                },
+                body: request.body,
+            });
+        } 
+        // 处理其他请求,使用JSON格式
+        else {
+            return fetch(`${dohjson}${new URL(request.url).search}`, {
+                headers: { 'Accept': jstontype }
+            });
+        }
+    }
 };
-
-async function handleRequest(request) {
-    // 使用Promise<Response>可以减少计费时间
-    let res = r404;
-    const { method, headers } = request
-    const {searchParams, pathname} = new URL(request.url)
-    
-    // 检查请求路径是否符合设定的前缀
-    if (!pathname.startsWith(path)) {
-        return r404;
-    }
-
-    // 处理GET请求,用于DNS查询
-    if (method === 'GET' && searchParams.has('dns')) {
-        res = fetch(`${doh}?dns=${searchParams.get('dns')}`, {
-            headers: { 'Accept': contype }
-        });
-    } 
-    // 处理POST请求,用于DNS查询
-    else if (method === 'POST' && headers.get('content-type') === contype) {
-        // 直接传输请求体,无需等待
-        res = fetch(doh, {
-            method: 'POST',
-            headers: {
-                'Accept': contype,
-                'Content-Type': contype,
-            },
-            body: request.body,
-        });
-    } 
-    // 处理其他请求,使用JSON格式
-    else {
-        res = fetch(`${dohjson}${new URL(request.url).search}`, {
-            headers: { 'Accept': jstontype }
-        });
-    }
-
-    return res;
-}
